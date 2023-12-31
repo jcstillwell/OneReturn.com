6b87e6d5c79069e560becdf51bdb573851c20b4b
Octal1021 <Hexidecimal21@outlook.com>
2023-12-31 16:46:36 -0700

    h

--- frontend/onereturnfe/src/pages/auth/Verify.js
+++ frontend/onereturnfe/src/pages/auth/Verify.js
@@ -1,8 +1,7 @@
 import axios from "axios";
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
-const Verify = async () => {
-
+const Verify = () => {
     const urlParams = new URLSearchParams(window.location.search);
     const token = urlParams.get('token');
     const [isVerified, setIsVerified] = useState(false);
@@ -10,20 +9,26 @@ const Verify = async () => {
     const [error, setError] = useState(false);
     const [errorMsg, setErrorMsg] = useState('');
 
-    try {
-        const response = await axios.get('https://onereturn.com/userapi/verify?token=${token}');
-        if(response.data.status === 'OK') {
-            setIsVerified(true);
-            setSuccessMsg(response.data.message);
-        } else {
-            setError(true);
-            setErrorMsg(response.data.message);
-        }
-    } catch (error) {
-        console.log(error); //REMOVE
-        setError(true);
-        setError(error.response.data.message);
-    }
+    useEffect(() => {
+        const verifyToken = async () => {
+            try {
+                const response = await axios.get(`https://onereturn.com/userapi/verify?token=${token}`);
+                if (response.data.status === 'OK') {
+                    setIsVerified(true);
+                    setSuccessMsg(response.data.message);
+                } else {
+                    setError(true);
+                    setErrorMsg(response.data.message);
+                }
+            } catch (error) {
+                console.error(error); // Log the error
+                setError(true);
+                setErrorMsg(error.response?.data?.message || "An error occurred");
+            }
+        };
+
+        verifyToken();
+    }, [token]);
 
     if (error) {
         return (
@@ -31,15 +36,13 @@ const Verify = async () => {
                 <h1 className="status">{errorMsg}</h1>
             </div>
         );
-    }
-    else if (isVerified) {
+    } else if (isVerified) {
         return (
             <div className="main">
                 <h1 className="status">{successMsg}</h1>
             </div>
         );
-    } 
-    else {
+    } else {
         return (
             <div className="main">
                 <h1 className="status">Please wait on this page until your email address is verified.</h1>
