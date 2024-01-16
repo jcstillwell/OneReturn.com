d4bf77a85b342d60d042399263cb5a93e981b4ed
Octal1021 <Hexidecimal21@outlook.com>
2024-01-15 19:08:57 -0700

    Working out merchant account verification

--- backend/onereturn/userapi/views.py
+++ backend/onereturn/userapi/views.py
@@ -336,7 +336,7 @@ class VerifyMerchant(APIView):
     def get(self, request):
         email = request.GET.get('primaryEmail')
         try:
-                query = MerchantAccount.objects.get(email=email)
+                query = MerchantAccount.objects.get(primaryEmailAddress=email)
                 return Response({'status':'OK', 'message':f'Successfully verified {query.email}! please return to previous screen to continue singing up', 'uuid':query.uuid}, status=status.HTTP_200_OK)
         except MerchantAccount.DoesNotExist:
                 return Response({'status':'ERROR', 'message':'Account has not been verified yet.'}) 
