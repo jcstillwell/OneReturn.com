e026b5d0bad5ae7ea0d97a0809c6229de310022b
Octal1021 <Hexidecimal21@outlook.com>
2024-01-16 09:32:54 -0700

    css changes

--- frontend/onereturnfe/src/pages/auth/Landing.js
+++ frontend/onereturnfe/src/pages/auth/Landing.js
@@ -20,24 +20,25 @@ const Landing = () => {
                 >
                     <h1>One place for all of your digital receipts, absolutely zero email sorting involved.</h1>
                 </motion.div>
+                <button className="main-trial-button" onClick={() => navigate('/preregister')}><h3>Try OneReturn for personal use.</h3></button>
             </div>
 
-            <button className="main-trial-button" onClick={() => navigate('/preregister')}><h3>Try OneReturn for personal use.</h3></button>
-
-
             <div className="for-business" id='for-business'>
-                <h1>Use OneReturn for your business</h1>
-                <h3>
-                    The average person has 1.75 email accounts, the average office worker receives 121 emails each day and, as of 2022, over 162 billion spam emails were sent everyday;
-                    odds are: your customer's digital receipts are getting lost.
-                </h3> 
-                <h3>
-                    OneReturn provides the all-in-one solution for getting your customers their receipts in an easy find, easy to manage way, on both your and the customers end.
-                    Not only are OneReturn receipts searchable by any and all fields, they are formatted in an easy to read way and dynamic so customers can see live updates, such as a return, refund
-                    or shipment status.
-                </h3>
-
-                <button className="main-trial-button"><a href='https://console.onereturn.com/merchantregistration'>Try OneReturn for business.</a></button>
+                    <div id='for-business-sect1'>
+                        <h1>Use OneReturn for your business</h1>
+                        <h3>
+                            The average person has 1.75 email accounts, the average office worker receives 121 emails each day and, as of 2022, over 162 billion spam emails were sent everyday;
+                            odds are: your customer's digital receipts are getting lost.
+                        </h3>
+                    </div>
+                    <div id='for-business-sect2'>
+                        <h3>
+                            OneReturn provides the all-in-one solution for getting your customers their receipts in an easy find, easy to manage way, on both your and the customers end.
+                            Not only are OneReturn receipts searchable by any and all fields, they are formatted in an easy to read way and dynamic so customers can see live updates, such as a return, refund
+                            or shipment status.
+                        </h3>
+                        <button className="main-trial-button"><a href='https://console.onereturn.com/merchantregistration'>Try OneReturn for business.</a></button>
+                    </div>
             </div>
 
             <div className="pricing">
