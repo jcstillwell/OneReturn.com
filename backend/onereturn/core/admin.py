1bc8a9ea413a9c98d62d21405ab9738fe5be088c
Octal1021 <Hexidecimal21@outlook.com>
2024-03-21 23:25:19 -0600

    just hoping anything works at this point

--- backend/onereturn/core/admin.py
+++ backend/onereturn/core/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 # Register your models here.
 from .models import AppUser
 
-@admin.register(AppUser)
+#@admin.register(AppUser)
 class AppUserAdmin(admin.ModelAdmin):
     list_display = ('email', 'first_name', 'last_name', 'is_active', 'emailVerified', 'phoneVerifed', 'regComplete')
     search_fields = ('email', 'first_name', 'last_name')
